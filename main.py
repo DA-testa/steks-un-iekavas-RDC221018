79e7acc1bbec148702b51e04ad4dbcf1b1818bcf
Kirils Bedins <125134888+RDC221018@users.noreply.github.com>
2023-02-14 17:24:09 +0000

    .

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next,i+1))
             #pass
 
-        elif next in ")]}":
-            if not opening_brackets_stack:
+        if next in ")]}":
+            # Process closing bracket, write your code here
+            #pass
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
-            top = opening_brackets_stack.pop()
-            if not are_matching(top.char, next):
-                return i+1
+            opening_brackets_stack.pop()
     if opening_brackets_stack:
-        return opening_brackets_stack[0].position
+        return opening_brackets_stack[-1].position
     return "Success"
 
 
